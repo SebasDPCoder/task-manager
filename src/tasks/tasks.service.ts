48623c3f87ff8c4f80a0019ed314574b7c008f3c
Sebastian <sebastian14027@gmail.com>
2025-11-20 22:33:16 -0500

    Upload base of the project

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from './entities/task.entity';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+  private countID = 1;
+
+  create(createTaskDto: CreateTaskDto) {
+    const newTask = {
+      id: this.countID++,
+      ...createTaskDto,
+      complete: false,
+    };
+
+    this.tasks.push(newTask);
+    return newTask
+  }
+
+  findAll() {
+    return this.tasks;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} task`;
+  }
+
+  update(id: number, updateTaskDto: UpdateTaskDto) {
+    return `This action updates a #${id} task`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} task`;
+  }
+}
